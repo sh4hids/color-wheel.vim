07df6344bed4811eec3b500e552312684326dfa7
Shahidul Islam Majumder <dev@shahid.pro>
2022-05-24 08:44:12 +0600

    fix: update horizon theme for lualine

--- lua/lualine/themes/horizon.lua
+++ lua/lualine/themes/horizon.lua
@@ -1,30 +1,30 @@
 
   local horizon = {
     inactive = {
-      a = { fg = '#1c1e26', bg = '#2e303e' , gui = 'bold' },
+      a = { fg = '#6c6f93', bg = '#2e303e' , gui = 'bold' },
       b = { fg = '#6c6f93', bg = '#232530' },
       c = { fg = '#6c6f93', bg = '#1c1e26' },
     },
     normal = {
-      a = { fg = '#1c1e26', bg = '#f43e5c' , gui = 'bold' },
-      b = { fg = '#1c1e26', bg = '#2e303e' },
+      a = { fg = '#6c6f93', bg = '#e95678' , gui = 'bold' },
+      b = { fg = '#6c6f93', bg = '#2e303e' },
       c = { fg = '#6c6f93', bg = '#1c1e26' },
     },
     insert = {
-      a = { fg = '#1c1e26', bg = '#21bfc2' , gui = 'bold' },
-      b = { fg = '#1c1e26', bg = '#2e303e' },
+      a = { fg = '#6c6f93', bg = '#21bfc2' , gui = 'bold' },
+      b = { fg = '#6c6f93', bg = '#2e303e' },
     },
     visual = {
-      a = { fg = '#1c1e26', bg = '#f09383' , gui = 'bold' },
-      b = { fg = '#1c1e26', bg = '#2e303e' },
+      a = { fg = '#6c6f93', bg = '#fac29a' , gui = 'bold' },
+      b = { fg = '#6c6f93', bg = '#2e303e' },
     },
     replace = {
-      a = { fg = '#1c1e26', bg = '#b877db' , gui = 'bold' },
-      b = { fg = '#1c1e26', bg = '#2e303e' },
+      a = { fg = '#6c6f93', bg = '#b877db' , gui = 'bold' },
+      b = { fg = '#6c6f93', bg = '#2e303e' },
     },
     command = {
-      a = { fg = '#1c1e26', bg = '#09f7a0' , gui = 'bold' },
-      b = { fg = '#1c1e26', bg = '#2e303e' },
+      a = { fg = '#6c6f93', bg = '#09f7a0' , gui = 'bold' },
+      b = { fg = '#6c6f93', bg = '#2e303e' },
     },
   }
 
