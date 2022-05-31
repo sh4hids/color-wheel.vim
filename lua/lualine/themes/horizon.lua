31ed2a0c2f264b1fbb30d382bb18aa111925784f
Shahidul Islam Majumder <dev@shahid.pro>
2022-05-31 07:03:24 +0600

    fix: refactor horizon theme

--- lua/lualine/themes/horizon.lua
+++ lua/lualine/themes/horizon.lua
@@ -7,27 +7,27 @@
     },
     normal = {
       a = { fg = '#1c1e26', bg = '#e95678' , gui = 'bold' },
-      b = { fg = '#1c1e26', bg = '#fac29a' },
+      b = { fg = '#1c1e26', bg = '#f09383' },
       c = { fg = '#6c6f93', bg = '#1a1c23' },
     },
     insert = {
       a = { fg = '#1c1e26', bg = '#25b2bc' , gui = 'bold' },
-      b = { fg = '#1c1e26', bg = '#fac29a' },
+      b = { fg = '#1c1e26', bg = '#f09383' },
       c = { fg = '#6c6f93', bg = '#1a1c23' },
     },
     visual = {
       a = { fg = '#1c1e26', bg = '#f09383' , gui = 'bold' },
-      b = { fg = '#1c1e26', bg = '#fac29a' },
+      b = { fg = '#1c1e26', bg = '#f09383' },
       c = { fg = '#6c6f93', bg = '#1a1c23' },
     },
     replace = {
       a = { fg = '#1c1e26', bg = '#f43e5c' , gui = 'bold' },
-      b = { fg = '#1c1e26', bg = '#fac29a' },
+      b = { fg = '#1c1e26', bg = '#f09383' },
       c = { fg = '#6c6f93', bg = '#1a1c23' },
     },
     command = {
       a = { fg = '#1c1e26', bg = '#09f7a0' , gui = 'bold' },
-      b = { fg = '#1c1e26', bg = '#fac29a' },
+      b = { fg = '#1c1e26', bg = '#f09383' },
       c = { fg = '#6c6f93', bg = '#1a1c23' },
     },
   }
